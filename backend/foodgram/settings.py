beb2236c7d3de914e9822f09b13f1d3374fe92a1
Pavel Senator <papoken@yandex.ru>
2024-11-02 19:02:43 +0300

    fix flake8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': 'db'
+        'HOST': 'db',
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
