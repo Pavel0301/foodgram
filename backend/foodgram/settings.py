e6d40dc841e9f0b8cab6dc69052eafe4cd9fc1b1
Pavel Senator <papoken@yandex.ru>
2024-11-03 18:00:17 +0300

    fix settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
+        'PORT': os.getenv('DB_PORT', default=5432),
     }
 }
 
@@ -122,6 +122,9 @@ REST_FRAMEWORK = {
 }
 
 DJOSER = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
     'LOGIN_FIELD': 'email',
     'PERMISSIONS': {
         'user': ['rest_framework.permissions.AllowAny'],
