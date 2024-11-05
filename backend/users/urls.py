ea8dd45f8b4dc60c05baa0d5870f582ace59e112
Pavel Senator <papoken@yandex.ru>
2024-11-05 14:47:20 +0300

    fix main.yml

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework import routers
 
@@ -11,8 +9,3 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
